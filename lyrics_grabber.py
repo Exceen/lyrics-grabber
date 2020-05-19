7321dd075ef6d739e664acd5799d9cd8322fe2b6
Exceen <github@exceen.at>
2020-05-19 15:33:37 +0200

    Added binary search
    iTunes.tracks() should return the list of tracks sorted alphabetically by artist, with some exceptions like 'The' or 'A', which are ignored by the sorting algorithm. With the newly added binary search it should be much quicker to find songs in such a list. Sorting exceptions are partly considered currently.

--- lyrics_grabber.py
+++ lyrics_grabber.py
@@ -37,12 +37,72 @@ def load(url):
     req = urllib2.Request(url, headers={'User-Agent': 'lyrics browser'})
     return urllib2.urlopen(req).read()
 
+
+def binary_search(arr, target, considerPrefixes=False):
+    return __binary_search(arr, 0, len(arr), target, considerPrefixes)
+
+def __binary_search(arr, low, high, x, considerPrefixes=False):
+    prefixes = ['The', 'A', '\.'] #regex
+
+    x_wo_prefix = x
+    if considerPrefixes:
+        x_wo_prefix = remove_prefix(x_wo_prefix, prefixes)
+
+    if high >= low: 
+        mid = low + (high - low) / 2
+
+        if arr[mid].artist() == x or (x_wo_prefix != x and remove_prefix(arr[mid].artist(), prefixes) == x_wo_prefix):
+            return mid 
+
+        # If element is smaller than mid, then it can only 
+        elif arr[mid].artist() > x or (x_wo_prefix != x and remove_prefix(arr[mid].artist(), prefixes) > x_wo_prefix): 
+            return __binary_search(arr, low, mid - 1, x, considerPrefixes) 
+
+        # Else the element can only be present in right subarray 
+        else: 
+            return __binary_search(arr, mid + 1, high, x, considerPrefixes) 
+  
+    else: 
+        # Element is not present in the array 
+        if not considerPrefixes:
+            return binary_search(arr, x, True)
+        return -1
+
+def remove_prefix(text, prefixes):
+    for prefix in prefixes:
+        text = re.sub('^' + prefix + ' ?', '', text)
+    return text
+
 def get_tracks(iTunes):
-    query = raw_input('Artist: ')
+    query = ''
+    while len(query) == 0:
+        query = raw_input('Artist: ')
     query_tracks = []
-    for track in iTunes.tracks():
-        if track.mediaKind() == mediaKindMusic and query in track.artist():
-            if len(track.lyrics()) == 0:
+    all_tracks = iTunes.tracks()
+
+    result = binary_search(all_tracks, query)
+    if result != -1:
+
+        start_idx = result
+        while all_tracks[start_idx].artist() == query:
+            start_idx -= 1
+        start_idx += 1
+
+        end_idx = result
+        while all_tracks[end_idx].artist() == query:
+            end_idx += 1
+
+
+        for i in xrange(start_idx, end_idx):
+            track = all_tracks[i]
+            if track.mediaKind() == mediaKindMusic and len(track.lyrics()) == 0:
+                query_tracks.append(track)
+
+    if len(query_tracks) == 0:
+        print 'Unable to find tracks via binary search. Searching iterative, this may take a while.'
+
+        for track in iTunes.tracks():
+            if track.mediaKind() == mediaKindMusic and len(track.lyrics()) == 0 and query in track.artist():
                 query_tracks.append(track)
     return query_tracks
 
@@ -61,11 +121,14 @@ def clean_raw_html(raw_html):
 def get_lyrics_for_track(track):
     url_template = 'https://www.azlyrics.com/lyrics/%s/%s.html'
 
-    url = url_template % (prepare_for_url(track.artist()), prepare_for_url(track.name()))
-
-    # print url
-    content = load(url)
-
+    content = ''
+    try:
+        url = url_template % (prepare_for_url(track.artist()), prepare_for_url(track.name()))
+        content = load(url)
+    except Exception as e:
+        url = url_template % (prepare_for_url(track.artist().split('The')[1]), prepare_for_url(track.name()))
+        content = load(url)
+    
     regex = 'Usage of azlyrics\.com content.*?\-\-\>((?:.|\n)*?)\<\/div\>'
     lyrics = re.findall(regex, content)[0]
     return clean_raw_html(lyrics)
